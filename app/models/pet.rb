04e2cf36693361b132ecb87f26c1bbdb4b6015e0
phuonganhdt-1773 <dinh.thi.phuong.anh@framgia.com>
2019-04-03 16:45:59 +0700

    search pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,7 @@
 class Pet < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
-  PET_PARAMS = [:name, :description].freeze
+  PET_PARAMS = [:name, :description, :term].freeze
 
   validates :name, presence: true, length: {maximum: Settings.max_name_lenght}
   validates :description, presence: true,
@@ -9,4 +9,12 @@ class Pet < ApplicationRecord
 
   scope :by_lastest, -> {order created_at: :desc}
   scope :all_pets, ->{select :id, :name}
+
+  def self.search term
+      if term
+        where("name LIKE ? OR description LIKE ?", "%#{term}%", "%#{term}%")
+      else
+        all
+      end
+    end
 end
